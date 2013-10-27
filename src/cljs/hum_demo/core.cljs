bc6692ed45446fc8d90d8af77a666e9c53059237
Matt Gauger <dev+mathias@bendyworks.com>
2013-10-26 22:57:56 -0500

    Start with the README's example code

--- src/cljs/hum_demo/core.cljs
+++ src/cljs/hum_demo/core.cljs
@@ -4,6 +4,18 @@
             [dommy.core :as dommy])
   (:use-macros [dommy.macros :only [node sel sel1]]))
 
+(def ctx (hum/create-context))
+(def vco (hum/create-osc ctx :sawtooth))
+(def vcf (hum/create-biquad-filter ctx))
+(def output (hum/create-gain ctx))
+
+(hum/connect vco vcf)
+(hum/connect vcf output)
+
+(hum/start-osc ctx vco)
+
+(hum/connect output (.-destination ctx))
+
 
 (defn setup []
   ;; do other stuff
